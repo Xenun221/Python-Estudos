7514bc51366ea14fd23f4dec2259a73a88d0f238
Gabriel Nunes <101987835+Xenun221@users.noreply.github.com>
2024-10-08 10:13:57 -0300

    Aula06a TiposPrimitivos
    Aula06 Explicando os tipos primitivos e como usa-los

---
+++ Aula06a.py
@@ -0,0 +1,17 @@
+"""
+Tipos primitivos:
+int = numeros inteiros(6,10,5)
+float = numeros reais(5.5, 10.5, 4.5)
+boal = True or False
+str= Palavras("Tech", "Gustavo")
+"""
+
+#Fazendo a soma de dois valores
+n1 = int(input("Digite um numero: "))
+n2 = int(input("Digite um numero: "))
+s = n1 + n2
+print("A soma de {} + {} e igual a {}".format(n1, n2, s))
+
+#Testando o tipo
+print(type(n1))
+
