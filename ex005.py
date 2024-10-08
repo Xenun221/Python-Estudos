3d25d3cbf08b80151c56cdaec12b604797340a12
Gabriel Nunes <101987835+Xenun221@users.noreply.github.com>
2024-10-08 13:27:59 -0300

    ex005
    exercicio05 antecessor e sucessor

---
+++ ex005.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia um numero inteiro e mostre na tela o seu sucesso e seu antecessor
+n1 = int(input("Digite o numero: "))
+print("Seu numero é: {}".format(n1))
+print("Seu antecessor é {}".format(n1 -1))
+print("Seu sucessor é {}".format(n1 + 1))
