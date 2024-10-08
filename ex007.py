678f714672c0939ff94152842043b6d9457bc83c
Gabriel Nunes <101987835+Xenun221@users.noreply.github.com>
2024-10-08 13:35:07 -0300

    ex007
    Exercicio07 Media Arithmetical

---
+++ ex007.py
@@ -0,0 +1,5 @@
+#Desenvolva um programa que leia as duas notas de um aluno e calcule e mostre sua média
+n1 = float(input("Digite o valor da sua primeira nota: "))
+n2 = float(input("Digite o valor da sua segunda nota: "))
+media = (n1+n2)/2
+print("A sua primeira nota foi {} e a segunda foi {} , sua media e de {}".format(n1, n2, media))
