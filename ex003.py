4e0a5176e316dd9af5ddb6d86d5c93b69cc0aec9
Gabriel Nunes <101987835+Xenun221@users.noreply.github.com>
2024-10-08 10:43:28 -0300

    Ex003
    Exercicio02 somando dois numeros e mostrando os resultados na tela

---
+++ ex003.py
@@ -0,0 +1,5 @@
+#Cire um programa que leia a soma de dois numeros e mostre a soma entre eles
+n1 = float(input("Digite o valor do primeiro numero: "))
+n2 = float(input("Digite o valor do segundo numero: "))
+soma = n1 + n2
+print("A soma entre {} e {} e igual a {}".format(n1,n2,soma))
