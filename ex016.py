ec5a171027a502cf9be0df4f98c6f216afef48fd
Gabriel Nunes <101987835+Xenun221@users.noreply.github.com>
2024-10-21 05:12:55 -0300

    ex016
    Exercicio016 Usando import Math

---
+++ ex016.py
@@ -0,0 +1,4 @@
+from math import trunc
+numreal = float(input("Digite um numero: "))
+trunca = trunc(numreal)
+print("O numero digitado foi {} e a sua proporção inteira é de {}".format(numreal, trunca))
